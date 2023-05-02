717c3f1252fff7016c9325a1f352d71326badadc
Vinícius Felizatti <87664450+viniciusfjacinto@users.noreply.github.com>
2023-05-02 18:40:11 -0300

    implements in bets_headxhead.ipynb

--- notebook/functions.py
+++ notebook/functions.py
@@ -18,13 +18,13 @@ from unidecode import unidecode
 payload = {}
 
 headers = {
-    'x-rapidapi-key': 'YOUR_API_KEY',
-    'x-rapidapi-host': 'v3.football.api-sports.io'
+    "x-rapidapi-key": "SUA_CHAVE_AQUI",
+    "x-rapidapi-host": "v3.football.api-sports.io",
 }
 
 
 def req_padrao(req, iterador):
-    '''
+    """
     tanto as funções req_padrao, req_alt e req_odds agilizam o processo de fazer as requisições à API, já entregando os dados de forma estruturada para manipulação
 
     args: req - caminho para o endpoint da requisição
@@ -33,58 +33,47 @@ def req_padrao(req, iterador):
 
     returns:
 
-    '''
+    """
     ls_v = []
 
     fixrange = req + str(iterador)
-    fix_d = requests.request("GET",
-                             fixrange,
-                             headers=headers,
-                             data=payload)
+    fix_d = requests.request("GET", fixrange, headers=headers, data=payload)
     fix_d = fix_d.json()
-    fix_d = json_normalize(fix_d['response'])
+    fix_d = json_normalize(fix_d["response"])
     ls_v.append(fix_d)
 
     return pd.concat(ls_v)
 
 
 def req_alt(req, iterador):
-
     ls_v = []
 
     fixrange = req + str(iterador)
-    fix_d = requests.request("GET",
-                             fixrange,
-                             headers=headers,
-                             data=payload)
+    fix_d = requests.request("GET", fixrange, headers=headers, data=payload)
     fix_d = fix_d.json()
-    fix_d = json_normalize(fix_d['response'])
-    fix_d['fixture.id'] = iterador
+    fix_d = json_normalize(fix_d["response"])
+    fix_d["fixture.id"] = iterador
     ls_v.append(fix_d)
 
     return pd.concat(ls_v)
 
 
 def req_hxh(req, iterador1, iterador2):
-
     ls_v = []
 
     fixrange = req + str(iterador1)
-    fix_d = requests.request("GET",
-                             fixrange,
-                             headers=headers,
-                             data=payload)
+    fix_d = requests.request("GET", fixrange, headers=headers, data=payload)
     fix_d = fix_d.json()
-    fix_d = json_normalize(fix_d['response'])
-    fix_d['fixture.id.ns'] = iterador2
+    fix_d = json_normalize(fix_d["response"])
+    fix_d["fixture.id.ns"] = iterador2
     ls_v.append(fix_d)
 
     return pd.concat(ls_v)
 
 
 def flatten_nested_json_df(df):
-    '''
-    abre um Json até o último nível, trazendo todas as colunas para o nível mais alto, 
+    """
+    abre um Json até o último nível, trazendo todas as colunas para o nível mais alto,
     no processo, repete alguns valores para as linhas
     args:
         df (DataFrame): dataframe contendo colunas em Json
@@ -94,7 +83,7 @@ def flatten_nested_json_df(df):
     notes:
         em certos Json com muitas informações, pode ocorrer um MemoryError. Recomenda combinar a função com
         .explode() e json_normalize(record_path = [], meta = []) para melhores resultados
-    '''
+    """
 
     df = df.reset_index()
     s = (df.applymap(type) == list).all()
@@ -104,7 +93,7 @@ def flatten_nested_json_df(df):
     while len(list_columns) > 0 or len(dict_columns) > 0:
         new_columns = []
         for col in dict_columns:
-            horiz_exploded = pd.json_normalize(df[col]).add_prefix(f'{col}.')
+            horiz_exploded = pd.json_normalize(df[col]).add_prefix(f"{col}.")
             horiz_exploded.index = df.index
             df = pd.concat([df, horiz_exploded], axis=1).drop(columns=[col])
             new_columns.extend(horiz_exploded.columns)
@@ -119,7 +108,7 @@ def flatten_nested_json_df(df):
 
 
 def transform_fixture(df):
-    '''
+    """
     utilizado no output das requisições de partidas/fixtures
     realiza a abertura e manipulação para o formato desejado para as colunas
     statistics, players, lineups e events
@@ -133,26 +122,29 @@ def transform_fixture(df):
         [1] = lineups
         [2] = players
         [3] = statistics
-    '''
+    """
 
     # abre o json da coluna 'statistics'
     statistics = []
 
-    for i, j in zip(range(df.shape[0]), df['fixture.id']):
+    for i, j in zip(range(df.shape[0]), df["fixture.id"]):
         try:
-            st = json_normalize(df['statistics'].iloc[i],
-                                record_path=['statistics'],
-                                meta=[['team', 'id'], ['team', 'name'],
-                                      ['team', 'logo']])
+            st = json_normalize(
+                df["statistics"].iloc[i],
+                record_path=["statistics"],
+                meta=[["team", "id"], ["team", "name"], ["team", "logo"]],
+            )
 
-            st = pd.pivot(st,
-                          index=['team.name', 'team.id', 'team.logo'],
-                          columns=['type'],
-                          values=['value']).reset_index()
+            st = pd.pivot(
+                st,
+                index=["team.name", "team.id", "team.logo"],
+                columns=["type"],
+                values=["value"],
+            ).reset_index()
 
             # st.index = st.reset_index()['index']
 
-            st['fixture.id'] = j
+            st["fixture.id"] = j
 
             statistics.append(st)
 
@@ -166,13 +158,12 @@ def transform_fixture(df):
 
     # renomeia as colunas removendo o multi_index 'value'
     cols = sts.columns
-    cols = [col[1] if col[0] == 'value' else col[0] for col in cols]
+    cols = [col[1] if col[0] == "value" else col[0] for col in cols]
     sts.columns = cols
 
     # transformar as variáveis BP e P% em numéricas
-    sts['Ball Possession'] = sts['Ball Possession'].str.strip(
-        '%').astype(float)
-    sts['Passes %'] = sts['Passes %'].str.strip('%').astype(float)
+    sts["Ball Possession"] = sts["Ball Possession"].str.strip("%").astype(float)
+    sts["Passes %"] = sts["Passes %"].str.strip("%").astype(float)
 
     # transforma as demais colunas em numéricas
     for i in sts.iloc[:, 3:].columns:
@@ -183,13 +174,17 @@ def transform_fixture(df):
     # abre o json da coluna 'players'
     players = []
 
-    for i, j in zip(range(df.shape[0]), df['fixture.id']):
+    for i, j in zip(range(df.shape[0]), df["fixture.id"]):
         try:
             pl = flatten_nested_json_df(
-                json_normalize(df['players'].iloc[i], ['players'],
-                               [['team', 'name'], ['team', 'id'], ['team', 'logo']]))
+                json_normalize(
+                    df["players"].iloc[i],
+                    ["players"],
+                    [["team", "name"], ["team", "id"], ["team", "logo"]],
+                )
+            )
 
-            pl['fixture.id'] = j
+            pl["fixture.id"] = j
 
             players.append(pl)
 
@@ -203,86 +198,118 @@ def transform_fixture(df):
 
     # ajustes nos dados de players
     # passes accuracy - transforma em variável numérica
-    pls['statistics.passes.accuracy'] = [
-        str(value).replace("%", "") if value != 'None' else value
-        for value in pls['statistics.passes.accuracy']
+    pls["statistics.passes.accuracy"] = [
+        str(value).replace("%", "") if value != "None" else value
+        for value in pls["statistics.passes.accuracy"]
     ]
-    pls['statistics.passes.accuracy'] = pls['statistics.passes.accuracy'].replace(
-        'None', np.nan).astype(float)
+    pls["statistics.passes.accuracy"] = (
+        pls["statistics.passes.accuracy"].replace("None", np.nan).astype(float)
+    )
 
     # passes accuracy - calcula a média por time
-    pls_pa = pls[['statistics.passes.accuracy', 'fixture.id', 'team.name', 'team.id']]\
-        .groupby(['fixture.id', 'team.name', 'team.id'], as_index=False)\
-        .mean().iloc[:, 3]
+    pls_pa = (
+        pls[["statistics.passes.accuracy", "fixture.id", "team.name", "team.id"]]
+        .groupby(["fixture.id", "team.name", "team.id"], as_index=False)
+        .mean()
+        .iloc[:, 3]
+    )
 
     # games rating - transforma em variável numérica - agrupa Goleiro dentro de Defesa
-    pls['statistics.games.rating'] = pls['statistics.games.rating'].replace(
-        '-', np.nan).replace('–', np.nan)
-    pls['statistics.games.rating'] = pls['statistics.games.rating'].astype(
-        float)
-    pls['statistics.games.position'] = pls[
-        'statistics.games.position'].str.replace('G', 'D')
+    pls["statistics.games.rating"] = (
+        pls["statistics.games.rating"].replace("-", np.nan).replace("–", np.nan)
+    )
+    pls["statistics.games.rating"] = pls["statistics.games.rating"].astype(float)
+    pls["statistics.games.position"] = pls["statistics.games.position"].str.replace(
+        "G", "D"
+    )
 
     # games rating - calcula a média por time e por grupo (F, D, M)
-    plrt = pls.groupby(['fixture.id', 'team.id', 'statistics.games.position'],
-                       as_index=False)[[
-                           'statistics.games.rating'
-                       ]].mean().sort_values(by=['fixture.id', 'team.id'])
-    plrt = plrt.pivot(values='statistics.games.rating',
-                      index=['fixture.id', 'team.id'],
-                      columns='statistics.games.position')
-    plrt.columns = [f'statistics.games.rating.{col}' for col in plrt.columns]
+    plrt = (
+        pls.groupby(
+            ["fixture.id", "team.id", "statistics.games.position"], as_index=False
+        )[["statistics.games.rating"]]
+        .mean()
+        .sort_values(by=["fixture.id", "team.id"])
+    )
+    plrt = plrt.pivot(
+        values="statistics.games.rating",
+        index=["fixture.id", "team.id"],
+        columns="statistics.games.position",
+    )
+    plrt.columns = [f"statistics.games.rating.{col}" for col in plrt.columns]
     try:
-        plrt = plrt.drop(columns='statistics.games.rating.SUB').reset_index()
+        plrt = plrt.drop(columns="statistics.games.rating.SUB").reset_index()
     except Exception:
         pass
 
     # games rating - calcula a média geral por time
-    pls_gr = pls[['statistics.games.rating', 'fixture.id', 'team.name', 'team.id']]\
-        .groupby(['fixture.id', 'team.name', 'team.id'], as_index=False)\
-        .mean().iloc[:, 3]
+    pls_gr = (
+        pls[["statistics.games.rating", "fixture.id", "team.name", "team.id"]]
+        .groupby(["fixture.id", "team.name", "team.id"], as_index=False)
+        .mean()
+        .iloc[:, 3]
+    )
 
     # filtra as variáveis de interesse
-    pls = pls.filter([
-        'team.id', 'team.name', 'team.logo', 'fixture.id', 'league_round',
-        'statistics.offsides', 'statistics.shots.total', 'statistics.shots.on',
-        'statistics.goals.total', 'statistics.goals.conceded',
-        'statistics.goals.assists', 'statistics.goals.saves',
-        'statistics.passes.total', 'statistics.passes.key',
-        'statistics.passes.accuracy', 'statistics.tackles.total',
-        'statistics.tackles.blocks', 'statistics.tackles.interceptions',
-        'statistics.duels.total', 'statistics.duels.won',
-        'statistics.dribbles.attempts', 'statistics.dribbles.success',
-        'statistics.dribbles.past', 'statistics.fouls.drawn',
-        'statistics.fouls.committed', 'statistics.cards.yellow',
-        'statistics.cards.red', 'statistics.penalty.won',
-        'statistics.penalty.commited', 'statistics.penalty.scored',
-        'statistics.penalty.missed', 'statistics.penalty.saved', 'statistics.games.rating'
-    ])
+    pls = pls.filter(
+        [
+            "team.id",
+            "team.name",
+            "team.logo",
+            "fixture.id",
+            "league_round",
+            "statistics.offsides",
+            "statistics.shots.total",
+            "statistics.shots.on",
+            "statistics.goals.total",
+            "statistics.goals.conceded",
+            "statistics.goals.assists",
+            "statistics.goals.saves",
+            "statistics.passes.total",
+            "statistics.passes.key",
+            "statistics.passes.accuracy",
+            "statistics.tackles.total",
+            "statistics.tackles.blocks",
+            "statistics.tackles.interceptions",
+            "statistics.duels.total",
+            "statistics.duels.won",
+            "statistics.dribbles.attempts",
+            "statistics.dribbles.success",
+            "statistics.dribbles.past",
+            "statistics.fouls.drawn",
+            "statistics.fouls.committed",
+            "statistics.cards.yellow",
+            "statistics.cards.red",
+            "statistics.penalty.won",
+            "statistics.penalty.commited",
+            "statistics.penalty.scored",
+            "statistics.penalty.missed",
+            "statistics.penalty.saved",
+            "statistics.games.rating",
+        ]
+    )
 
     # transforma as variáveis filtradas em float
     for i in pls.iloc[:, 3:].columns:
         pls[i] = pls[i].astype(float)
 
     # realiza a soma por time
-    pls = pls.groupby(['fixture.id', 'team.name', 'team.id'],
-                      as_index=False)\
-        .sum()
+    pls = pls.groupby(["fixture.id", "team.name", "team.id"], as_index=False).sum()
 
     # substitui a variável passes accuracy e games rating
-    pls['statistics.passes.accuracy'] = pls_pa
-    pls['statistics.games.rating'] = pls_gr
+    pls["statistics.passes.accuracy"] = pls_pa
+    pls["statistics.games.rating"] = pls_gr
 
     # faz um merge dos ratings por time/grupo
-    pls = pls.merge(plrt, on=['fixture.id', 'team.id'])
+    pls = pls.merge(plrt, on=["fixture.id", "team.id"])
 
     # abre o json da coluna 'lineups'
     lineups = []
 
-    for i, j in zip(range(df.shape[0]), df['fixture.id']):
+    for i, j in zip(range(df.shape[0]), df["fixture.id"]):
         try:
-            lp = json_normalize(df['lineups'].iloc[i])
-            lp['fixture.id'] = j
+            lp = json_normalize(df["lineups"].iloc[i])
+            lp["fixture.id"] = j
             lineups.append(lp)
 
         except Exception:
@@ -298,17 +325,17 @@ def transform_fixture(df):
     # abre o json da coluna 'events'
     events = []
 
-    for i, j in zip(range(df.shape[0]), df['fixture.id']):
+    for i, j in zip(range(df.shape[0]), df["fixture.id"]):
         try:
-            ev = (json_normalize(df['events'].iloc[i]))
+            ev = json_normalize(df["events"].iloc[i])
 
-            ev = ev.groupby(['team.id', 'detail'],
-                            as_index=False).count()
+            ev = ev.groupby(["team.id", "detail"], as_index=False).count()
 
-            ev = ev.pivot_table(index=['team.id'], columns='detail',
-                                values='type').reset_index()
+            ev = ev.pivot_table(
+                index=["team.id"], columns="detail", values="type"
+            ).reset_index()
 
-            ev['fixture.id'] = j
+            ev["fixture.id"] = j
 
             events.append(ev)
 
--- notebook/visualization.py
+++ notebook/visualization.py
@@ -1,53 +1,72 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-sns.set(style='white')
+
+sns.set(style="white")
 sns.set_palette("Set2")
 
 
 def viz_radar(df_predictions):
-    '''
+    """
     utilizado para visualizacao das previsões geradas pela API para uma partida em questão
 
     args:
         df (DataFrame): dataframe de previsoes
 
     returns:
-        radar: gráficos de diferença percentual entre variáveis do time 
+        radar: gráficos de diferença percentual entre variáveis do time
         'form, att, def, poisson, h2h' e previsão sugerida
         gráfico em formato radar/teia-de-aranha
-    '''
+    """
 
     from math import pi
 
-    df_predictions[['fixture.id', 'teams.home.id', 'teams.home.name',
-                    'teams.away.name', 'teams.away.id', 'predictions.advice']]
+    df_predictions[
+        [
+            "fixture.id",
+            "teams.home.id",
+            "teams.home.name",
+            "teams.away.name",
+            "teams.away.id",
+            "predictions.advice",
+        ]
+    ]
 
-    comp = pd.concat([df_predictions[['teams.home.name', 'teams.away.name']], df_predictions[['comparison.form.home',
-                                                                                              'comparison.form.away',
-                                                                                             'comparison.att.home',
-                                                                                              'comparison.att.away',
-                                                                                              'comparison.def.home',
-                                                                                              'comparison.def.away',
-                                                                                              'comparison.poisson_distribution.home',
-                                                                                              'comparison.poisson_distribution.away',
-                                                                                              'comparison.h2h.home',
-                                                                                              'comparison.h2h.away',
-                                                                                              'comparison.goals.home',
-                                                                                              'comparison.goals.away',
-                                                                                              'comparison.total.home',
-                                                                                              'comparison.total.away']]], axis=1)
+    comp = pd.concat(
+        [
+            df_predictions[["teams.home.name", "teams.away.name"]],
+            df_predictions[
+                [
+                    "comparison.form.home",
+                    "comparison.form.away",
+                    "comparison.att.home",
+                    "comparison.att.away",
+                    "comparison.def.home",
+                    "comparison.def.away",
+                    "comparison.poisson_distribution.home",
+                    "comparison.poisson_distribution.away",
+                    "comparison.h2h.home",
+                    "comparison.h2h.away",
+                    "comparison.goals.home",
+                    "comparison.goals.away",
+                    "comparison.total.home",
+                    "comparison.total.away",
+                ]
+            ],
+        ],
+        axis=1,
+    )
 
-    X = comp.filter(regex='.home')
-    X.columns = X.columns.str.replace('.home', '')
-    Y = comp.filter(regex='.away')
-    Y.columns = Y.columns.str.replace('.away', '')
+    X = comp.filter(regex=".home")
+    X.columns = X.columns.str.replace(".home", "")
+    Y = comp.filter(regex=".away")
+    Y.columns = Y.columns.str.replace(".away", "")
 
     compxy = pd.concat([X, Y])
     for i in compxy.iloc[:, 1:].columns:
-        compxy[i] = compxy[i].str.replace('%', '').astype(float)
+        compxy[i] = compxy[i].str.replace("%", "").astype(float)
 
-    compxy.columns = compxy.columns.str.replace('comparison.', '')
+    compxy.columns = compxy.columns.str.replace("comparison.", "")
 
     df = compxy
 
@@ -72,31 +91,39 @@ def viz_radar(df_predictions):
 
     # Draw ylabels
     ax.set_rlabel_position(0)
-    plt.yticks([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], [
-               "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"], color="grey", size=10)
+    plt.yticks(
+        [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+        ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"],
+        color="grey",
+        size=10,
+    )
     plt.ylim(0, 100)
 
     # Ind1
 
     values = df.iloc[0, 1:].values.flatten().tolist()
     values += values[:1]
-    ax.plot(angles, values, linewidth=1.2,
-            linestyle='solid', label=compxy.iloc[0, 0])
-    ax.fill(angles, values, 'b', alpha=0.1)
+    ax.plot(angles, values, linewidth=1.2, linestyle="solid", label=compxy.iloc[0, 0])
+    ax.fill(angles, values, "b", alpha=0.1)
 
     # Ind2
     values = df.iloc[1, 1:].values.flatten().tolist()
     values += values[:1]
-    ax.plot(angles, values, linewidth=1.2,
-            linestyle='solid', label=compxy.iloc[1, 0])
-    ax.fill(angles, values, 'r', alpha=0.1)
+    ax.plot(angles, values, linewidth=1.2, linestyle="solid", label=compxy.iloc[1, 0])
+    ax.fill(angles, values, "r", alpha=0.1)
 
     # Add legend
-    plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+    plt.legend(loc="upper right", bbox_to_anchor=(0.1, 0.1))
 
     # observation
-    plt.figtext(.97, 0.5, str(list(df_predictions['predictions.advice'])).replace(
-        '[', '').replace(']', '').replace("'", ""))
+    plt.figtext(
+        0.97,
+        0.5,
+        str(list(df_predictions["predictions.advice"]))
+        .replace("[", "")
+        .replace("]", "")
+        .replace("'", ""),
+    )
 
     # Show the graph
     radar = plt.show
@@ -105,7 +132,7 @@ def viz_radar(df_predictions):
 
 
 def viz_stacked(viz):
-    '''
+    """
     utilizado para visualizacao das estatísticas de confrontos anteriores entre 2 times
 
     args:
@@ -113,12 +140,44 @@ def viz_stacked(viz):
 
     returns:
         stacked: gráfico de barras empilhadas que vão de 0 à 100%, comparando alguns destaques dos times em seus últimos confrontos
-    '''
-    viz = viz[['teams.name', 'statistics.games.rating.pergame',
-               'Total Shots.pergols', 'Total Shots.pergame',
-               'Ball Possession.pergame', 'D', 'L', 'W', 'gols']].\
-        set_index('teams.name').rename(columns={'gols': 'Gols feitos', 'W': 'Vitórias', 'D': 'Empates', 'L': 'Derrotas', 'statistics.games.rating.pergame': 'Rating por Jogo',
-                                                'Ball Possession.pergame': 'Posse de Bola por Jogo', 'Total Shots.pergame': 'Chutes ao Gol por Jogo', 'Total Shots.pergols': 'Chutes ao Gol por Gol'}).T
+    """
+    viz = (
+        viz[
+            [
+                "teams.league.fixtures.loses.away",
+                "teams.league.fixtures.wins.away",
+                "teams.league.fixtures.loses.home",
+                "teams.league.fixtures.wins.home",
+                "teams.name",
+                "statistics.games.rating.pergame",
+                "Total Shots.pergols",
+                "Total Shots.pergame",
+                "Ball Possession.pergame",
+                "D",
+                "L",
+                "W",
+                "gols",
+            ]
+        ]
+        .set_index("teams.name")
+        .rename(
+            columns={
+                "teams.league.fixtures.wins.home": "Vitórias em Casa no Campeonato",
+                "teams.league.fixtures.loses.home": "Derrotas em Casa no Campeonato",
+                "teams.league.fixtures.wins.away": "Vitórias Fora de Casa no Campeonato",
+                "teams.league.fixtures.loses.away": "Derrotas Fora de Casa no Campeonato",
+                "gols": "Gols feitos",
+                "W": "Vitórias",
+                "D": "Empates",
+                "L": "Derrotas",
+                "statistics.games.rating.pergame": "Rating por Jogo",
+                "Ball Possession.pergame": "Posse de Bola por Jogo",
+                "Total Shots.pergame": "Chutes ao Gol por Jogo",
+                "Total Shots.pergols": "Chutes ao Gol por Gol",
+            }
+        )
+        .T
+    )
 
     # Add a small value to all the values in the dataframe to avoid zero values
     viz = viz + 0.00001
@@ -128,28 +187,33 @@ def viz_stacked(viz):
 
     # Create a stacked bar graph
     fig, ax = plt.subplots()
-    viz_pct.plot(kind='barh', stacked=True, ax=ax)
+    viz_pct.plot(kind="barh", stacked=True, ax=ax)
 
     # Set the x-axis limits and ticks
     ax.set_xlim([0, 1])
     ax.set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
-    ax.set_xticklabels(['0%', '20%', '40%', '60%', '80%', '100%'])
+    ax.set_xticklabels(["0%", "20%", "40%", "60%", "80%", "100%"])
 
     # Add the values to the graph
     for i, (idx, row) in enumerate(viz_pct.iterrows()):
         cum_sum = 0
         for j, val in enumerate(row):
             if val != 0:
-                ax.text(cum_sum + val / 2, i,
-                        f'{float(viz.values[i][j]):.0f}', ha='center', va='center')
+                ax.text(
+                    cum_sum + val / 2,
+                    i,
+                    f"{float(viz.values[i][j]):.0f}",
+                    ha="center",
+                    va="center",
+                )
             cum_sum += val
 
     # Set the y-axis label and legend
-    ax.set_ylabel('')
-    ax.legend(title='Results', loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.set_ylabel("")
+    ax.legend(title="Results", loc="center left", bbox_to_anchor=(1, 0.5))
 
     # Set the title of the plot
-    ax.set_title('Results by Team')
+    ax.set_title("Results by Team")
 
     # Show the graph
     stacked = plt.show
