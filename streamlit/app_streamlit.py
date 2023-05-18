277be21004612a69da57c576f7a66750f1437fd8
Vinícius Felizatti <87664450+viniciusfjacinto@users.noreply.github.com>
2023-05-18 19:28:01 -0300

    created app_streamlit.py

---
+++ streamlit/app_streamlit.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+from pandas import json_normalize
+import requests
+#from ..notebook.functions import *
+
+@st.cache
+def main():
+
+    if __name__ == '__main__':
+        main()
+
+st.title("Previsão Esportiva - Futebol")
+
+#API KEY
+
+# request API
+payload = {}
+
+headers = {
+    "x-rapidapi-key": "c58a251d827eeee96ac5cb890e882cc9",
+    "x-rapidapi-host": "v3.football.api-sports.io",
+}
+
+#API CALL - caminho para as requisicoes
+stream_req = 'https://v3.football.api-sports.io/fixtures?date='
+fix_req = "https://v3.football.api-sports.io/fixtures?ids="
+odds_req = 'https://v3.football.api-sports.io/odds?fixture='
+pred_req = 'https://v3.football.api-sports.io/predictions?fixture='
+hxh_req = 'https://v3.football.api-sports.io/fixtures/headtohead?h2h='
+
+#cria um range de datas para +- X dias
+
+from datetime import datetime
+date = datetime.today().strftime('%Y-%m-%d')
+
+#seleciona a range de datas
+import datetime
+x_dias_inicio = 0
+x_dias_fim = 7
+
+#date = hoje, datet0 = hoje - X dias, datet1 = hoje + X dias
+datet0 = (datetime.date.today()) + datetime.timedelta(days=-x_dias_inicio)
+datet1 = (datetime.date.today()) + datetime.timedelta(days=+x_dias_fim)
+
+date_list = (pd.date_range(start=datet0, end=datet1,
+                           freq='D')).strftime('%Y-%m-%d').tolist()
+
+def req_padrao(req, iterador):
+
+    ls_v = []
+
+    fixrange = req + str(iterador)
+    fix_d = requests.request("GET", fixrange, headers=headers, data=payload)
+    fix_d = fix_d.json()
+    fix_d = json_normalize(fix_d["response"])
+    ls_v.append(fix_d)
+
+    return pd.concat(ls_v)
+
+df_stream = pd.concat([req_padrao(stream_req,i) for i in date_list])
+
+# Function to transform the date format
+def transform_date(date_string):
+    return date_string.split('T')[0]
+
+# Apply the transformation to the 'fixture.date' column
+df_stream['fixture.date'] = df_stream['fixture.date'].apply(transform_date)
+
+# Display a selectbox to choose the city
+selected_country = st.selectbox('Selecione um país', df_stream['league.country'].sort_values().unique())
+
+selected_league = st.selectbox('Select uma liga', df_stream[df_stream['league.country']==selected_country]['league.name'].sort_values().unique())
+
+# Filter the dataframe based on the selected city
+filtered_df = df_stream[(df_stream['league.country'] == selected_country) & (df_stream['league.name'] == selected_league)][['fixture.date','league.name','teams.home.name','teams.away.name']].rename(columns = {"fixture.date":"Data","league.name":"Liga","teams.home.name":"Time Casa", "teams.away.name":"Time Visitante"})
+
+# Display the filtered dataframe
+st.dataframe(filtered_df)
